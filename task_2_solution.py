4a09336679c49eeee3eb475d4c71326727bcefac
Pavel Usachev <devheart@mail.ru>
2022-08-08 21:26:34 +0300

    another try

--- task_2_solution.py
+++ task_2_solution.py
@@ -10,7 +10,8 @@ def calculate_target_ratio(x, target_name):
     return round(x[target_name].mean(), 2)
 
 def calculate_data_dtypes(x):
-    return x.dtypes.count()
+    return [x.dtypes[x.dtypes != 'object'].count(),
+            x.dtypes[x.dtypes == 'object'].count()]
 
 def calculate_cheap_apartment(x):
     return round((x.price_doc <= 10 ** 6).sum())
@@ -25,7 +26,7 @@ def calculate_mean_squared_by_num_rooms(x):
     return round(x.groupby('num_room').full_sq.mean(), 2)
 
 def calculate_squared_stats_by_material(x):
-    return round(x.groupby('material').price_doc.agg(['min', 'max']).rename(columns={"min": "amin", "max": "amax"}), 2)
+    return round(x.groupby('material').price_doc.agg(['max', 'min']).rename(columns={"min": "amin", "max": "amax"}), 2)
 
 def calculate_crosstab(x):
     return round(x.pivot_table('price_doc', index=['sub_area'], columns=['product_type'], aggfunc='mean').fillna(0), 2)
