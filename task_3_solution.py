f15d2928ef9eb57197bf8eefac9f25d4a03be442
Pavel Usachev <devheart@mail.ru>
2022-08-14 00:51:45 +0300

    Fix scale

--- task_3_solution.py
+++ task_3_solution.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.base import TransformerMixin
 from sklearn.model_selection import train_test_split
 
 def split_data_into_two_samples(df: pd.DataFrame,
@@ -13,10 +14,11 @@ def prepare_data(df: pd.DataFrame):
     return (df.select_dtypes([np.number]).dropna(axis=1).drop(columns=["price_doc", "id"]),
             df["price_doc"])
 
-def scale_data(df: pd.DataFrame, transformer):
-    return transformer.fit_transform(df)
+def scale_data(df: pd.DataFrame, transformer: TransformerMixin):
+    scaled = transformer.fit_transform(df)
+    return pd.DataFrame(scaled, columns=df.columns)
 
-def prepare_data_for_model(df: pd.DataFrame, transformer):
+def prepare_data_for_model(df: pd.DataFrame, transformer: TransformerMixin):
     X_train, y_train = prepare_data(df)
     X_train_scaled = scale_data(X_train, transformer)
     return X_train_scaled, y_train
