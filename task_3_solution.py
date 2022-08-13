ccb71af73c66e39435512466fc4733ad270f964e
Pavel Usachev <devheart@mail.ru>
2022-08-13 17:44:11 +0300

    chagnes

--- task_3_solution.py
+++ task_3_solution.py
@@ -23,21 +23,10 @@ def prepare_data_for_model(df: pd.DataFrame, transformer):
     X_train_scaled = scale_data(X_train, transformer)
     return X_train_scaled, y_train
 
-X_train, X_test = split_data_into_two_samples(data)
-X_train_scaled, y_train = prepare_data_for_model(X_train, StandardScaler())
-X_test_scaled, y_test = prepare_data_for_model(X_test, StandardScaler())
-
 def fit_first_linear_model(x_train, y_train):
     model = LinearRegression()
     model.fit(x_train, y_train)
     return model
-
-model1 = fit_first_linear_model(X_train_scaled, y_train)
-
-X_train, X_test = split_data_into_two_samples(data)
-X_train_scaled, y_train = prepare_data_for_model(X_train, MinMaxScaler())
-
-model2 = fit_first_linear_model(X_train_scaled, y_train)
 
 def evaluate_model(model: LinearRegression, X_test, y_test):
     y_pred = model.predict(X_test)
@@ -45,12 +34,8 @@ def evaluate_model(model: LinearRegression, X_test, y_test):
             mean_absolute_error(y_test, y_pred),
             r2_score(y_test, y_pred)]]
 
-evaluate_model(model1, X_test_scaled, y_test)
-
 def calculate_model_weights(model: LinearRegression, feature_names):
     indicies = [X_train.columns.get_loc(feature) for feature in feature_names]
     weights = model.coef_[indicies]
     sorted_weights = sorted(zip(weights, feature_names), reverse=True)
     return sorted_weights
-
-calculate_model_weights(model1, ["full_sq", "floor"])
