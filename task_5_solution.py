979565c3d07623bced1257ae296693240322c2ba
Pavel Usachev <devheart@mail.ru>
2022-09-21 01:05:19 +0300

    task 4-6

--- task_5_solution.py
+++ task_5_solution.py
@@ -26,3 +26,27 @@ def model_with_optimal_depth(X, y):
         scores[score] = max_depth
 
     return scores[max(scores.keys())]
+
+def model_with_optimal_samples(X, y):
+    fill_value = -9999
+    cv = KFold(n_splits=5, random_state=27, shuffle=True)
+
+    scores = {}
+    for leaf_count in [5, 10, 15, 25, 50, 100, 150, 250, 500, 1000, 2500, 5000, 10000]:
+        model = DecisionTreeClassifier(random_state=1, max_depth=10, max_leaf_nodes=leaf_count)
+        score = cross_val_score(cv=cv, X=X.fillna(fill_value), y=y, estimator=model).mean()
+        scores[score] = leaf_count
+
+    return scores[max(scores.keys())]
+
+def model_with_optimal_features(X, y):
+    fill_value = -9999
+    cv = KFold(n_splits=5, random_state=27, shuffle=True)
+
+    scores = {}
+    for features in range(1, X.shape[1]):
+        model = DecisionTreeClassifier(random_state=1, max_depth=10, max_leaf_nodes=100, max_features=features)
+        score = cross_val_score(cv=cv, X=X.fillna(fill_value), y=y, estimator=model).mean()
+        scores[score] = features
+
+    return scores[max(scores.keys())]
