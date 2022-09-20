e6b0371791b16660c90a5d5138e4263dfcf08074
Pavel Usachev <devheart@mail.ru>
2022-09-21 01:30:46 +0300

    2 task + 7 task

--- task_5_solution.py
+++ task_5_solution.py
@@ -2,11 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import KFold, cross_val_score, GridSearchCV
 
+# 1
 def prepare_data(df: pd.DataFrame):
     return [df.drop(columns=['isFraud', 'TransactionID']), df.isFraud]
 
+# 2
 def fit_first_model(X, y, x_test, y_test):
     train_x, validation_x, train_y, validation_y = train_test_split(X.fillna(0), y.fillna(0), train_size=0.7, random_state=1, shuffle=True)
     model = DecisionTreeClassifier(random_state=1)
@@ -15,6 +17,17 @@ def fit_first_model(X, y, x_test, y_test):
     test_score = roc_auc_score(model.predict(x_test.fillna(0)), y_test.fillna(0))
     return [round(validation_score, 4), round(test_score, 4)]
 
+# 3
+def fit_second_model(X, y, x_test, y_test):
+    fill_value = -9999
+    train_x, validation_x, train_y, validation_y = train_test_split(X.fillna(fill_value), y, train_size=0.7, random_state=1, shuffle=True)
+    model = DecisionTreeClassifier(random_state=1)
+    model.fit(train_x, train_y)
+    validation_score = roc_auc_score(model.predict(validation_x), validation_y)
+    test_score = roc_auc_score(model.predict(x_test.fillna(fill_value)), y_test)
+    return [round(validation_score, 4), round(test_score, 4)]
+
+# 4
 def model_with_optimal_depth(X, y):
     fill_value = -9999
     cv = KFold(n_splits=5, random_state=27, shuffle=True)
@@ -27,6 +40,7 @@ def model_with_optimal_depth(X, y):
 
     return scores[max(scores.keys())]
 
+# 5
 def model_with_optimal_samples(X, y):
     fill_value = -9999
     cv = KFold(n_splits=5, random_state=27, shuffle=True)
@@ -39,6 +53,7 @@ def model_with_optimal_samples(X, y):
 
     return scores[max(scores.keys())]
 
+# 6
 def model_with_optimal_features(X, y):
     fill_value = -9999
     cv = KFold(n_splits=5, random_state=27, shuffle=True)
@@ -50,3 +65,17 @@ def model_with_optimal_features(X, y):
         scores[score] = features
 
     return scores[max(scores.keys())]
+
+# 7
+def model_with_optimal_params(X, y):
+    cv = KFold(n_splits=5, random_state=27, shuffle=True)
+    gs = GridSearchCV(DecisionTreeClassifier(random_state = 1),
+                      cv = cv,
+                      param_grid = {
+                          'max_features': range(1, X.shape[1]),
+                          'max_depth': range(3, 12),
+                          'min_samples_leaf': [5, 10, 15, 25, 50, 100, 150, 250, 500, 1000, 2500, 5000, 10000],
+                          'criterion': ['entropy', 'gini']
+                      },
+                      scoring='r2')
+    return gs.fit(X.fillna(-9999), y)
