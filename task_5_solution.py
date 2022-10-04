76534799e8eb98691437c0f2b0cfdd80efe9950f
Pavel Usachev <devheart@mail.ru>
2022-10-04 08:39:39 +0300

    tasks 8,9

--- task_5_solution.py
+++ task_5_solution.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.tree import DecisionTreeClassifier
@@ -78,4 +79,39 @@ def model_with_optimal_params(X, y):
                           'criterion': ['entropy', 'gini']
                       },
                       scoring='r2')
-    return gs.fit(X.fillna(-9999), y)
+    gs.fit(X.fillna(-9999), y)
+    return gs.cv_results_['params']
+
+# 8
+def fit_final_model(X, y, x_test, y_test):
+    fill_value = -9999
+    train_x, validation_x, train_y, validation_y = train_test_split(X.fillna(fill_value), y, train_size=0.7, random_state=1, shuffle=True)
+    model = DecisionTreeClassifier(random_state=1, max_features=13, max_depth=11, min_samples_leaf=5)
+    model.fit(train_x, train_y)
+    validation_score = roc_auc_score(model.predict(validation_x), validation_y)
+    test_score = roc_auc_score(model.predict(x_test.fillna(fill_value)), y_test)
+    return [round(validation_score, 4), round(test_score, 4)]
+
+#9
+def check_tree_stability(X, y, x_test, y_test):
+    fill_value = -9999
+    val_scores = np.array([])
+    test_scores = np.array([])
+    for i in range(5):
+        indecies = np.random.randint(X.shape[0], size=int(X.shape[0]))
+        leng = round(len(indecies) * 0.9)
+        match = indecies[:leng]
+        not_match = indecies[leng:]
+
+        tr_x = X.fillna(fill_value).iloc[match]
+        tr_y = y.iloc[match]
+
+        val_x = X.fillna(fill_value).iloc[not_match]
+        val_y = y.iloc[not_match]
+
+        model = DecisionTreeClassifier(random_state=1, max_features=13, max_depth=11, min_samples_leaf=5)
+        model.fit(tr_x, tr_y)
+        val_scores = np.append(val_scores, roc_auc_score(model.predict(val_x), val_y))
+        test_scores = np.append(test_scores, roc_auc_score(model.predict(x_test.fillna(fill_value)), y_test))
+
+    return [val_scores.mean(), val_scores.std(), test_scores.mean(), test_scores.std()]
